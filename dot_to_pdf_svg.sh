4091ad2eec87cdf309951a072e47fcbf523bf3ec
anne04 <anne.06.cse@gmail.com>
2025-03-04 17:30:08 -0500

    Update dot_to_pdf_svg.sh - prints

--- dot_to_pdf_svg.sh
+++ dot_to_pdf_svg.sh
@@ -9,10 +9,10 @@ else
 	echo "converting to a temporary tmp file"
 	cat $1 | sed 's/L:\([^ ]\+\), R:/\1-/g'   | sed 's/label="[0-9][^"]*"/label=""/g' | awk -F'=' '{ if ($1 == "penwidth") {print $1 "=" ($2 ^ 6) ","} else {print $0 }}'   | tr '\n' ' '   | sed "s/;/\n/g"  > tmp
 
-	echo "converting to pdf file"
+	echo "converting to svg file"
 	cat tmp   | dot -Kneato -n -y -Tsvg -Efontname="Arial" -Nlabel="" -Nwidth=1 -Nheight=1 -Epenwidth=20 -Nstyle="filled"> edge_graph.svg #-Npenwidth=20 
 
-	echo "converting to a svg file"
+	echo "converting to a pdf file"
 	cat tmp   | dot -Kneato -n -y -Tpdf -Efontname="Arial" -Nlabel="" -Nwidth=1 -Nheight=1 -Epenwidth=20 -Nstyle="filled"> edge_graph.pdf #-Npenwidth=20 
 
 	echo "Deleting the temporary temp file"
